d6524f66a79bb9648ae319b2b7ec32e1ccf5c780
Lucas Campos <lucascampos.ads@gmail.com>
2020-09-24 21:18:15 -0300

    doc: adicionado components e pages

--- clone-github/src/App.tsx
+++ clone-github/src/App.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Header from './components/Header';
+import Profile from './pages/Profile';
+import Repo from './pages/Repo';
 
 function App() {
   return (
-    <h1>Rocketseat</h1>
+    <BrowserRouter>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Profile />} />
+        <Route path="/:username" element={<Profile />} />
+        <Route path="/:username/:reponame" element={<Repo />} />
+      </Routes>
+
+      {/* <Footer /> */}
+    </BrowserRouter> >
   );
 }
 
