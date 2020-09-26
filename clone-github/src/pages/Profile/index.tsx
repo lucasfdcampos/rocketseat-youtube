5d80fb705f67e376673162a221d2647cd841de12
Lucas Campos <lucascampos.ads@gmail.com>
2020-09-26 12:37:51 -0300

    feat: Adiciona RepoCards

--- clone-github/src/pages/Profile/index.tsx
+++ clone-github/src/pages/Profile/index.tsx
@@ -1,31 +1,52 @@
-import React from 'react';
+import React from "react";
 
-import { Container, Main, LeftSide, RightSide } from './styles';
+import { Container, Main, LeftSide, RightSide, Repos } from "./styles";
 
-import ProfileData from '../../components/ProfileData';
+import ProfileData from "../../components/ProfileData";
+import RepoCard from "../../components/RepoCard";
 
 const Profile: React.FC = () => {
   return (
     <Container>
       <Main>
         <LeftSide>
-          <ProfileData 
-            username={'lucasfdcampos'}
-            name={'Lucas Campos'}
-            avatarUrl={'https://avatars0.githubusercontent.com/u/56896330?v=4'}
+          <ProfileData
+            username={"lucasfdcampos"}
+            name={"Lucas Campos"}
+            avatarUrl={"https://avatars0.githubusercontent.com/u/56896330?v=4"}
             followers={311}
             following={7}
-            company={'Entrepreneur'}
-            location={'Paraná, Brazil'}
-            email={'lucascampos.ads@gmail.com'}
-            blog={'https://www.linkedin.com/in/lucasfdcampos/'}
+            company={"Entrepreneur"}
+            location={"Paraná, Brazil"}
+            email={"lucascampos.ads@gmail.com"}
+            blog={"https://www.linkedin.com/in/lucasfdcampos/"}
           />
         </LeftSide>
-        
-        <RightSide></RightSide>
+
+        <RightSide>
+          <Repos>
+            <h2>Random Repos</h2>
+
+            <div>
+              {[1, 2, 3, 4, 5, 6].map(n => (
+                <RepoCard
+                  key={n}
+                  username={'lucasfdcampos'}
+                  reponame={'rocketseat-bootcamp-gostack-bonus-cursos'}
+                  description={
+                    'Repository dedicated to Rocketseat Gostack Bootcamp bonus (reward) courses.'
+                  }
+                  language={n % 3 === 0 ? 'JavaScript' : 'TypeScript'}
+                  stars={8}
+                  forks={1}
+                />
+              ))}
+            </div>
+          </Repos>
+        </RightSide>
       </Main>
     </Container>
   );
-}
+};
 
 export default Profile;
