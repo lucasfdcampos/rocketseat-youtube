8456049b0adf35842f9a7e1598811e1228515465
Lucas Campos <lucascampos.ads@gmail.com>
2020-09-26 15:50:07 -0300

    feat: Altera Profile

--- clone-github/src/pages/Profile/styles.ts
+++ clone-github/src/pages/Profile/styles.ts
@@ -1,10 +1,12 @@
 import styled from "styled-components";
+import { RiBookMarkLine } from "react-icons/ri";
 
 export const Container = styled.div`
   --horizontalPadding: 16px;
   --verticalPadding: 24px;
 
   padding: var(--verticalPadding) var(--horizontalPadding);
+  overflow: hidden;
 `;
 
 export const Main = styled.div`
@@ -37,7 +39,7 @@ export const RightSide = styled.div`
 
 export const Repos = styled.div`
   margin-top: var(--verticalPadding);
-  
+
   > h2 {
     font-size: 16px;
     font-weight: normal;
@@ -51,7 +53,7 @@ export const Repos = styled.div`
 
     grid-template-columns: 1fr;
 
-    @media(min-width: 768px) {
+    @media (min-width: 768px) {
       grid-template-columns: 1fr 1fr;
       grid-auto-rows: minmax(min-content, max-content);
     }
@@ -62,4 +64,73 @@ export const CalendarHeading = styled.span`
   font-size: 16px;
   margin: 36px 0 9px;
   display: inline-flex;
+`;
+
+export const RepoIcon = styled(RiBookMarkLine)`
+  width: 16px;
+  height: 16px;
+  margin-right: 4px;
+`;
+
+export const Tab = styled.div`
+  background: var(--primary);
+
+  .content {
+    display: flex;
+    align-items: center;
+    width: min-content;
+
+    padding: 14px 16px;
+
+    border-bottom: 2px solid var(--orange);
+
+    .label {
+      font-size: 14px;
+      padding: 0 7px;
+      font-weight: 600;
+    }
+    .number {
+      font-size: 12px;
+      background: var(--ticker);
+      padding: 2px 6px;
+      border-radius: 24px;
+    }
+  }
+
+  .line {
+    display: flex;
+    width: 200vw;
+    border-bottom: 1px solid var(--border);
+    margin-left: -50vw;
+  }
+
+  &.mobile {
+    margin-top: var(--verticalPadding);
+
+    .content {
+      margin: 0 auto;
+    }
+
+    @media (min-width: 768px) {
+      display: none;
+    }
+  }
+  &.desktop {
+    display: none;
+
+    @media (min-width: 768px) {
+      display: unset;
+
+      .wrapper {
+        display: flex;
+        margin: 0 auto;
+        max-width: 1280px;
+      }
+
+      .offset { 
+        width: 25%;
+        margin-right: var(--horizontalPadding);
+      }
+    }
+  }
 `;
