2934612a4f1a4ece3f2a854b3408cbdb0f7ca366
Lucas Campos <lucascampos.ads@gmail.com>
2020-09-26 10:22:24 -0300

    feat: Adiciona Profile

--- clone-github/src/pages/Profile/index.tsx
+++ clone-github/src/pages/Profile/index.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { Container, Main, LeftSide, RightSide } from './styles';
+
+import ProfileData from '../../components/ProfileData';
 
 const Profile: React.FC = () => {
-  return <div />;
+  return (
+    <Container>
+      <Main>
+        <LeftSide>
+          <ProfileData 
+            username={'lucasfdcampos'}
+            name={'Lucas Campos'}
+            avatarUrl={'https://avatars0.githubusercontent.com/u/56896330?v=4'}
+            followers={311}
+            following={7}
+            company={'Entrepreneur'}
+            location={'Paraná, Brazil'}
+            email={'lucascampos.ads@gmail.com'}
+            blog={'https://www.linkedin.com/in/lucasfdcampos/'}
+          />
+        </LeftSide>
+        
+        <RightSide></RightSide>
+      </Main>
+    </Container>
+  );
 }
 
 export default Profile;
