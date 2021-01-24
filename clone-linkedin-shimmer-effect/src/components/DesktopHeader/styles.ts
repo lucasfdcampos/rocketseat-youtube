cb372e2a04a58b6a2071a95e8e4fef3aa8304895
Lucas Campos <lucascampos.ads@gmail.com>
2021-01-24 20:45:02 -0300

    Estilizacao

---
+++ clone-linkedin-shimmer-effect/src/components/DesktopHeader/styles.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const Container = styled.div``;
+
+export const Wrapper = styled.div``;
+
+export const LinkedInIcon = styled.div``;
+
+export const SearchInput = styled.div``;
+
+export const HomeIcon = styled.div``;
+
+export const NotificationsIcon = styled.div``;
+
+export const ProfileCircle = styled.img``;
+
+export const CaretDownIcon = styled.div``;
+
+
+// 20:07 do video
---
+++ clone-linkedin-shimmer-effect/src/components/Layout/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import MobileHeader from '../MobileHeader';
+import DesktopHeader from '../DesktopHeader';
+import LeftColumn from '../LeftColumn';
+import MiddleColumn from '../MiddleColumn';
+import RightColumn from '../RightColumn';
+
+import { Container } from './styles';
+
+const Layout: React.FC = () => {
+  return (
+    <Container>
+      <MobileHeader />
+      <DesktopHeader />
+
+      <main>
+        <LeftColumn />
+        <MiddleColumn />
+        <RightColumn />
+      </main>
+    </Container>
+  );
+};
+
+export default Layout;
