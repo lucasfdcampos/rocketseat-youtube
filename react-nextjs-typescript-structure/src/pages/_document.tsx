6c6ff4c10046cc40fe9a569325826a96cbb70887
Lucas Campos <lucascampos.ads@gmail.com>
2021-02-07 20:58:26 -0300

    feat: Next-images

--- react-nextjs-typescript-structure/src/pages/_document.tsx
+++ react-nextjs-typescript-structure/src/pages/_document.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
-import Document, { DocumentContext, DocumentInitialProps } from 'next/document';
+import Document, {
+  DocumentContext,
+  DocumentInitialProps,
+  Html,
+  Main,
+  NextScript,
+} from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
+import Head from 'next/head';
 
 export default class MyDocument extends Document {
   static async getInitialProps(
@@ -28,5 +35,26 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal();
     }
+  }
+
+  render(): JSX.Element {
+    return (
+      <Html lang="pt">
+        <Head>
+          <meta charSet="utf-8" />
+
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700"
+            rel="stylesheet"
+          />
+
+          <link rel="icon" href="https://rocketseat.com.br/favicon.ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
--- react-nextjs-typescript-structure/src/pages/index.tsx
+++ react-nextjs-typescript-structure/src/pages/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Head from 'next/head';
 
+import RocketseatLogo from '../assets/rocketseat.svg';
+
 const Home: React.FC = () => {
   return (
     <div>
@@ -9,6 +11,7 @@ const Home: React.FC = () => {
       </Head>
 
       <main>
+        <RocketseatLogo />
         <h1>Hello World</h1>
       </main>
     </div>
