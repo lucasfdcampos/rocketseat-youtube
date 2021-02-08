c5e41c4a7de439a1d02b5d6468cc53a21723f932
Lucas Campos <lucascampos.ads@gmail.com>
2021-02-07 21:10:06 -0300

    feat: Index page

--- react-nextjs-typescript-structure/src/pages/index.tsx
+++ react-nextjs-typescript-structure/src/pages/index.tsx
@@ -2,19 +2,19 @@ import React from 'react';
 import Head from 'next/head';
 
 import RocketseatLogo from '../assets/rocketseat.svg';
+import { Container } from '../styles/pages/Home';
 
 const Home: React.FC = () => {
   return (
-    <div>
+    <Container>
       <Head>
         <title>Homepage</title>
       </Head>
 
-      <main>
-        <RocketseatLogo />
-        <h1>Hello World</h1>
-      </main>
-    </div>
+      <RocketseatLogo />
+      <h1>ReactJS Structure</h1>
+      <p>A ReactJS + Next.js structure made by Rocketseat.</p>
+    </Container>
   );
 };
 
