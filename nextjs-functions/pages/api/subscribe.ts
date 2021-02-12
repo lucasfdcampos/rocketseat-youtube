68c8176b0e3b843847e6f76543b0c5df501d2fab
Lucas Campos <lucascampos.ads@gmail.com>
2021-02-12 18:52:46 -0300

    Function servless post (add a new subscriber) on DB

--- nextjs-functions/pages/api/subscribe.ts
+++ nextjs-functions/pages/api/subscribe.ts
@@ -2,7 +2,13 @@ import { NowRequest, NowResponse } from '@vercel/node';
 import { MongoClient, Db } from 'mongodb';
 import url from 'url';
 
+let cachedDb: Db = null;
+
 async function connectToDatabase(uri: string) {
+  if (cachedDb) {
+    return cachedDb;
+  }
+
   const client = await MongoClient.connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -11,10 +17,23 @@ async function connectToDatabase(uri: string) {
   const dbName = url.parse(uri).pathname.substr(1); // '/news' => 'news'
 
   const db = client.db(dbName);
+
+  cachedDb = db;
+
+  return db;
 }
 
-export default (request: NowRequest, response: NowResponse) => {
+export default async (request: NowRequest, response: NowResponse) => {
   const { email } = request.body;
 
-  return response.json({ message: `Hello ${email}` });
+  const db = await connectToDatabase(process.env.MONGODB_URI);
+
+  const collection = db.collection('subscribers');
+
+  await collection.insertOne({
+    email,
+    subscribedAt: new Date(),
+  });
+
+  return response.status(201).json({ ok: true });
 };
