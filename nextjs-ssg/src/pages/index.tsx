802ab08047e94bd84767c59699bf040ba272dcd7
Lucas Campos <lucascampos.ads@gmail.com>
2021-02-14 16:27:43 -0300

    feat: GetStaticProps

--- nextjs-ssg/src/pages/index.tsx
+++ nextjs-ssg/src/pages/index.tsx
@@ -1,5 +1,26 @@
-export default function Home() {
+import { GetStaticProps } from 'next';
+
+export default function Home({ org }) {
   return (
-    <h1>Hello World, Nextjs.</h1>
-  )
+    <div>
+      <h1>{org.login}</h1>
+      <h3>{org.description}</h3>
+
+      <p>
+        Site: <a href={org.blog}>{org.blog}</a>
+      </p>
+    </div>
+  );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await fetch('https://api.github.com/orgs/rocketseat');
+  const data = await response.json();
+
+  return {
+    props: {
+      org: data,
+    },
+    revalidate: 10,
+  };
+};
