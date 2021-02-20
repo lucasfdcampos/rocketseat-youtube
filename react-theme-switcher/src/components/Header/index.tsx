b8f320555225d1fc14385b64be5a6c6fb25de16d
Lucas Campos <lucascampos.ads@gmail.com>
2021-02-19 21:31:01 -0300

    feat: Add switch

--- react-theme-switcher/src/components/Header/index.tsx
+++ react-theme-switcher/src/components/Header/index.tsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import Switch from 'react-switch';
+import { ThemeContext } from 'styled-components';
+import { shade } from 'polished';
 
 import { Container } from './styles';
 
 const Header: React.FC = () => {
-  return <Container>Hello World</Container>;
+  const { colors } = useContext(ThemeContext);
+
+  return (
+    <Container>
+      Hello World
+      <Switch
+        onChange={() => {}}
+        checked={true}
+        checkedIcon={false}
+        uncheckedIcon={false}
+        height={10}
+        width={40}
+        handleDiameter={20}
+        offColor={shade(0.15, colors.primary)}
+        onColor={colors.secundary}
+      />
+    </Container>
+  );
 };
 
 export default Header;
