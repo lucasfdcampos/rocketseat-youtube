f0c10066e47b32bb5cc163c97a5b03ee8cdf4814
Lucas Campos <lucascampos.ads@gmail.com>
2021-02-28 21:56:21 -0300

    Permission camera

--- react-pwa-expo/src/pages/Camera.tsx
+++ react-pwa-expo/src/pages/Camera.tsx
@@ -1,8 +1,25 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Text, View } from 'react-native';
 import { Camera as ExpoCamera } from 'expo-camera';
 
 const Camera: React.FC = () => {
+  const [hasPermission, setHasPermission] = useState(false);
+  const [type, setType] = useState(ExpoCamera.Constants.Type.back);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await ExpoCamera.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
   return (
     <View style={{ flex: 1 }}>
       <ExpoCamera style={{ flex: 1 }} />
